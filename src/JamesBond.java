198a9c4a5b8a80397c01ae5684da9e08db10bc35
Schurter.Caitlyn <Schurter.Caitlyn@SEMS-259-33.SEMS259.LAB>
2018-11-07 16:13:08 -0800

    Vault & JamesBond   11/7/18

--- src/JamesBond.java
+++ src/JamesBond.java
@@ -1,9 +1,22 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class JamesBond {
-public static void main(String[] args) {
-	Vault v = new Vault();
-	v.setVaultCode(665);
-	v.tryCode(556);
-	
+	@Test
+	public void findCode() {
+		Vault v = new Vault();
+		v.setVaultCode(665);
+		v.tryCode(556);
+		assertEquals(false, v.tryCode(556));
+		assertEquals(true, v.tryCode(665));
+	}
 }
-}
+/*
+ * Minions stuart = new Minions("Stuart", 1, "yellow", "");
+ * assertEquals("Stuart", stuart.getName()); assertEquals(1, stuart.getEyes());
+ * assertEquals("yellow", stuart.getColor());
+ * 
+ * Minions dave = new Minions("Dave", 2, "yellow", ""); assertEquals("Dave",
+ * dave.getName()); assertEquals(2, dave.getEyes()); assertEquals("yellow",
+ * dave.getColor()); }
+ */
--- src/Vault.java
+++ src/Vault.java
@@ -20,5 +20,5 @@ public class Vault {
 		else {
 			return false;
 		}
-	}	
+	}
 }
