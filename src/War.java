d65e82f6285c2664277ee2d897c92903ca9a5751
Schurter.Caitlyn <Schurter.Caitlyn@SEMS-259-33.SEMS259.LAB>
2018-11-14 16:30:22 -0800

    Custom class: War, ScoreKeeper, Deck   11/14/18

---
+++ src/War.java
@@ -0,0 +1,19 @@
+
+public class War {
+	public static void main(String[] args) {
+		testDeck();
+
+	}
+
+	private static void testDeck() {
+		Deck d = new Deck();
+		System.out.println(d);
+		d.shuffle();
+		System.out.println(d);
+		int card = d.dealCard();
+		System.out.print(card + " ");
+		card = d.dealCard();
+		System.out.print(card + " ");
+	}
+
+}
